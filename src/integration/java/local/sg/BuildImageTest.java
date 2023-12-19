4e1eab3c3bd04bc9a8d7c0ba681afedbf2e4a720
Etienne Koekemoer <me@etiennek.com>
2023-12-19 20:54:28 +0100

    Small cleanup

--- src/integration/java/local/sg/BuildImageTest.java
+++ src/integration/java/local/sg/BuildImageTest.java
@@ -1,6 +1,5 @@
 package local.sg;
 
-import org.gradle.testkit.runner.GradleRunner;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.web.reactive.server.WebTestClient;
